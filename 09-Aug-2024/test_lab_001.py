234fbc3fda11ed254dccea0abc9c1d7e9dde1793
gantagit <gvk9786@hotmail.com>
2024-08-14 11:18:36 +0100

    09-Aug-2024

---
+++ 09-Aug-2024/test_lab_001.py
@@ -0,0 +1,13 @@
+import time
+
+from selenium import webdriver
+
+url = "https://app.vwo.com"
+
+
+def test_vwo_login():
+    driver = webdriver.Chrome()
+    driver.get(url)
+    # assert driver.title == "Login - "
+    assert driver.title == "Login - VWO"
+    time.sleep(10)
