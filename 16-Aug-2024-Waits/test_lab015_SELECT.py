9dd6f27e8d7594a4d004b54d07aba0adbd754af6
gantagit <gvk9786@hotmail.com>
2024-08-21 17:10:25 +0100

    16-Aug-2024 Concepts of Selenium

--- 16-Aug-2024-Waits/test_lab015_SELECT.py
+++ 16-Aug-2024-Waits/test_lab015_SELECT.py
@@ -9,9 +9,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 from allure_commons.types import AttachmentType
 
-url = "https://katalon-demo-cura.herokuapp.com/"
-login_url = "https://katalon-demo-cura.herokuapp.com/profile.php#login"
-error_text = "Login failed! Please ensure the username and password are valid."
+from selenium.webdriver.support.ui import Select
+
+url = "https://the-internet.herokuapp.com/dropdown"
 
 
 @pytest.mark.postive
@@ -19,3 +19,10 @@ def test_vwo_login_select():
     driver = webdriver.Chrome()
     driver.get(url)
 
+    choose_option = driver.find_element(By.ID, "dropdown")
+    select_option = Select(choose_option)
+
+    time.sleep(3)
+    select_option.select_by_visible_text("Option 1")
+
+    time.sleep(5)
